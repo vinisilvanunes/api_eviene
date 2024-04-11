5b08450b4c75afbeebc17cf423394d47731a612a
vinisilvanunes <viniciusdasilva.nunes01@gmail.com>
2024-04-11 12:58:43 -0300

    Adicionando login de usuário

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
 const router = express.Router();
@@ -45,6 +46,34 @@ router.post('/register', async (req,res)=>{
   }catch(error){
     return res.status(500).json({message: "Erro ao cadastrar o usuário, tente novamente mais tarde!"});
   }
-})
+});
+
+router.post('/login', async (req, res)=>{
+  const {email, password} = req.body;
+
+  if(!email || !password){
+    return res.status(400).json({message: "Preencha todos os campos"});
+  }
+
+  const user = await User.findOne({email: email});
+  if(!user){
+    return res.status(400).json({message: "Usuário não encontrado"});
+  }
+
+  const checkPassword = await bcrypt.compare(password, user.password);
+  if(!checkPassword){
+    return res.status(400).json({message: "Usuário ou senha incorreto"});
+  }
+
+  try{
+    const secret = process.env.SECRET;
+    const token = jwt.sign({
+      id: user.id
+    }, secret)
+    return res.status(200).json({message: "Login realizado com sucesso", token, id: user.id});
+  }catch(error){
+    return res.status(500).json({message: "Erro ao logar o usuário, tente novamente mais tarde!"});
+  }
+});
 
 module.exports = router;
