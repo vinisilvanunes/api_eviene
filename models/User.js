1a58f42c8ed085d6100804693231135225cbb2dd
vinisilvanunes <viniciusdasilva.nunes01@gmail.com>
2024-04-11 12:38:28 -0300

    Adicionando model do usuário

---
+++ models/User.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  username: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  birthDate: {
+    type: Date,
+    required: true
+  },
+  profilePicture: {
+    type: String,
+    default: ""
+  },
+  bio: {
+    type: String,
+    default: ""
+  },
+  posts: {
+    type: [{type:Schema.Types.ObjectId, ref: 'Post'}],
+    default: []
+  },
+  eventAttended: {
+    type: [{type:Schema.Types.ObjectId, ref: 'Event'}],
+    default: []
+  },
+  followers: {
+    type: [{type:Schema.Types.ObjectId, ref: 'Follower'}],
+    default: []
+  },
+  following: {
+    type: [{type:Schema.Types.ObjectId, ref: 'Following'}],
+    default: []
+  },
+  active: {
+    type: Boolean,
+    default: true
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
