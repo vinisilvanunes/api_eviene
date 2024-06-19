825104f7e9b5cb03a74ed75f38938e0ba6278b35
vinisilvanunes <viniciusdasilva.nunes01@gmail.com>
2024-06-19 14:53:54 -0300

    fix: integrando com o serviço s3

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,15 +1,32 @@
 const multer = require('multer');
+const multerS3 = require('multer-s3');
+const { S3Client } = require('@aws-sdk/client-s3');
+const mime = require('mime-types');
 
+require('dotenv').config();
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads/'); 
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + '-' + file.originalname);
+const s3 = new S3Client({
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
   }
 });
 
-const upload = multer({ storage: storage }).array('images');
 
-module.exports = upload
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: process.env.S3_BUCKET_NAME,
+    acl: 'public-read',
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    metadata: function (req, file, cb) {
+      cb(null, { fieldName: file.fieldname });
+    },
+    key: function (req, file, cb) {
+      cb(null,Date.now().toString() + '-' + file.originalname);
+    }
+  })
+}).array('images', 10);
+
+module.exports = upload;
